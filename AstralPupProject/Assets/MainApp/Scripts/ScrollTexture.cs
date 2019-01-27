b87803447fbe88062da0d1ccfe4e4eef4aab1507
JwHanson <jw.hanson@outlook.com>
2019-01-27 00:18:32 -0700

    Added waterfall and stick pickup script

---
+++ AstralPupProject/Assets/MainApp/Scripts/ScrollTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+
+    public float moveTextureSpeedX;
+    public float moveTextureSpeedY;
+    float moveX;
+    float moveY;
+
+    void Update()
+    {
+        moveX = (moveTextureSpeedX * Time.deltaTime) + moveX;
+        moveY = (moveTextureSpeedY * Time.deltaTime) + moveY;
+        if (moveX > 1) moveX = 0;
+        if (moveY > 1) moveY = 0;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(moveX, moveY);
+    }
+}
