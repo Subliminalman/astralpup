feda5dc7f2d6c09aebb3ec5428d5384aa3673fa0
Subliminalman <ryan.leonski@gmail.com>
2019-01-27 10:33:47 -0700

    Added Grandma and speech bubble

---
+++ AstralPupProject/Assets/MainApp/Scripts/Bubble.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    void Update () {
+        Camera target = Camera.main;
+
+
+        if (target) {
+            transform.LookAt (target.transform, Vector3.up);
+        }
+
+
+    }
+}
