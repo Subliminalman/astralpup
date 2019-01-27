4aad8630722c0ee955477fbd4531799592b77825
Connorses <3219086+octoConnors@users.noreply.github.com>
2019-01-27 13:45:16 -0700

    Added camera scaling script
    Also DOTWeen is there but I didn't wind up using it but it's going in irregardless

---
+++ AstralPupProject/Assets/MainApp/Scripts/Environment/TreeFallTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeFallTrigger : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Falling Tree Hitbox");
+        animator.Play("TreeFall");
+
+    }
+}
